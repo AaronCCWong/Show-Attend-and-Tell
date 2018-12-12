ead6aab7ba18123d5c72e4f85a3b52200b7419ac
AaronCCWong <aarn.wong@gmail.com>
2018-12-11 21:09:09 -0500

    Revert changes for sending sorted inputs to decoder

--- train.py
+++ train.py
@@ -71,19 +71,20 @@ def train(epoch, encoder, decoder, optimizer, cross_entropy_loss, data_loader, w
         captions_length = captions_length.squeeze(-1).cuda()
         img_features = encoder(imgs)
         optimizer.zero_grad()
+        preds, alphas = decoder(img_features, captions, captions_length)
 
         # need to sort to make pack_padded_sequence work
         sorted_lengths, idxs = captions_length.sort(descending=True)
         sorted_captions = captions[idxs]
-        sorted_features = img_features[idxs]
+        targets = sorted_captions[:, 1:]
+        sorted_preds = preds[idxs]
         # plus 1 for <start>
         sorted_lengths = [length + 1 for length in sorted_lengths]
 
-        preds, alphas = decoder(sorted_features, sorted_captions, sorted_lengths)
-        targets = sorted_captions[:, 1:]
-
-        targets = pack_padded_sequence(targets, sorted_lengths, batch_first=True)[0]
-        packed_preds = pack_padded_sequence(preds, sorted_lengths, batch_first=True)[0]
+        targets = pack_padded_sequence(
+            targets, sorted_lengths, batch_first=True)[0]
+        packed_preds = pack_padded_sequence(
+            sorted_preds, sorted_lengths, batch_first=True)[0]
 
         att_regularization = alpha_c * ((1 - alphas.sum(1))**2).mean()
 
@@ -94,7 +95,6 @@ def train(epoch, encoder, decoder, optimizer, cross_entropy_loss, data_loader, w
 
         total_caption_length = sum(sorted_lengths).float()
         acc1, acc5 = accuracy(packed_preds, targets, topk=(1, 5))
-
         losses.update(loss.item(), total_caption_length)
         top1.update(acc1[0], total_caption_length)
         top5.update(acc5[0], total_caption_length)
@@ -126,19 +126,20 @@ def validate(epoch, encoder, decoder, cross_entropy_loss, data_loader, word_dict
             imgs, captions = Variable(imgs).cuda(), Variable(captions).cuda()
             captions_length = captions_length.squeeze(-1).cuda()
             img_features = encoder(imgs)
+            preds, alphas = decoder(img_features, captions, captions_length)
 
             # need to sort to make pack_padded_sequence work
             sorted_lengths, idxs = captions_length.sort(descending=True)
             sorted_captions = captions[idxs]
-            sorted_features = img_features[idxs]
+            targets = sorted_captions[:, 1:]
+            sorted_preds = preds[idxs]
             # plus 1 for <start>
             sorted_lengths = [length + 1 for length in sorted_lengths]
 
-            preds, alphas = decoder(sorted_features, sorted_captions, sorted_lengths)
-            targets = sorted_captions[:, 1:]
-
-            targets = pack_padded_sequence(targets, sorted_lengths, batch_first=True)[0]
-            packed_preds = pack_padded_sequence(preds, sorted_lengths, batch_first=True)[0]
+            targets = pack_padded_sequence(
+                targets, sorted_lengths, batch_first=True)[0]
+            packed_preds = pack_padded_sequence(
+                sorted_preds, sorted_lengths, batch_first=True)[0]
 
             att_regularization = alpha_c * ((1 - alphas.sum(1))**2).mean()
 
@@ -147,7 +148,6 @@ def validate(epoch, encoder, decoder, cross_entropy_loss, data_loader, word_dict
 
             total_caption_length = sum(sorted_lengths).float()
             acc1, acc5 = accuracy(packed_preds, targets, topk=(1, 5))
-
             losses.update(loss.item(), total_caption_length)
             top1.update(acc1[0], total_caption_length)
             top5.update(acc5[0], total_caption_length)
@@ -156,14 +156,14 @@ def validate(epoch, encoder, decoder, cross_entropy_loss, data_loader, word_dict
                 caps = []
                 for caption in cap_set:
                     cap = [word_idx for word_idx in caption
-                                    if word_idx != word_dict['<start>'] and word_idx != word_dict['<pad>']]
+                           if word_idx != word_dict['<start>'] and word_idx != word_dict['<pad>']]
                     caps.append(cap)
                 references.append(caps)
 
-            word_idxs = torch.max(preds, dim=2)[1]
+            word_idxs = torch.max(sorted_preds, dim=2)[1]
             for idxs in word_idxs.tolist():
                 hypotheses.append([idx for idx in idxs
-                                       if idx != word_dict['<start>'] and idx != word_dict['<pad>']])
+                                   if idx != word_dict['<start>'] and idx != word_dict['<pad>']])
 
             if batch_idx % log_interval == 0:
                 print('Validation Batch: [{0}/{1}]\t'
@@ -177,7 +177,8 @@ def validate(epoch, encoder, decoder, cross_entropy_loss, data_loader, word_dict
 
         bleu_1 = corpus_bleu(references, hypotheses, weights=(1, 0, 0, 0))
         bleu_2 = corpus_bleu(references, hypotheses, weights=(0.5, 0.5, 0, 0))
-        bleu_3 = corpus_bleu(references, hypotheses, weights=(0.33, 0.33, 0.33, 0))
+        bleu_3 = corpus_bleu(references, hypotheses,
+                             weights=(0.33, 0.33, 0.33, 0))
         bleu_4 = corpus_bleu(references, hypotheses)
 
         writer.add_scalar('val_bleu1', bleu_1, epoch)
